3f1f0003e38390191b81d1dc1aea9118186320e1
Jaedson Silva <contacanal798@gmail.com>
2022-09-22 10:39:43 -0300

    adding method to get tasks from database

--- database.py
+++ database.py
@@ -18,3 +18,34 @@ class Database:
 
         data = request.json()
         return data.get('token')
+
+    def get_tasks(self) -> dict:
+        data = {
+            'path': 'devtasks/tasks/'
+        }
+
+        headers = {
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        request = requests.get(self.database_url + '/database', headers=headers, json=data)
+        tasks = {}
+
+        if request.status_code == 401:
+            self._token = self._login()
+            request = requests.get(self.database_url + '/database', headers=headers, json=data)
+
+            if request.status_code == 200:
+                data = request.json()
+                result = data.get('result')
+
+                if result:
+                    tasks = result
+        elif request.status_code == 200:
+            data = request.json()
+            result = data.get('result')
+
+            if result:
+                tasks = result
+
+        return tasks
