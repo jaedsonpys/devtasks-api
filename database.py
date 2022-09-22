b3ddf4a7a5bb105f4da95d117064afc476db50b2
Jaedson Silva <contacanal798@gmail.com>
2022-09-22 10:29:00 -0300

    set database url as instance attribute

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ import requests
 
 class Database:
     def __init__(self) -> None:
-        database_url = 'https://remote-cookiedb.herokuapp.com'
+        self.database_url = 'https://remote-cookiedb.herokuapp.com'
         database_pw = os.environ.get('DATABASE_PW')
 
-        request = requests.get(database_url + '/login', params={'password': database_pw})
+        request = requests.get(self.database_url + '/login', params={'password': database_pw})
 
         if request.status_code == 401:
             raise ConnectionError('Invalid database API password')
