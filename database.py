005c08aa0494021c29f64782cb735502be641198
Jaedson Silva <contacanal798@gmail.com>
2022-09-22 10:27:11 -0300

    logging into the database API

--- database.py
+++ database.py
@@ -7,3 +7,11 @@ class Database:
     def __init__(self):
         database_url = 'https://remote-cookiedb.herokuapp.com'
         database_pw = os.environ.get('DATABASE_PW')
+
+        request = requests.get(database_url + '/login', params={'password': database_pw})
+
+        if request.status_code == 401:
+            raise ConnectionError('Invalid database API password')
+
+        data = request.json()
+        self._token = data.get('token')
