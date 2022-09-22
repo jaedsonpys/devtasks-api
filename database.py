6570b5d0cba10772545a3cde2a3423c148845bcf
Jaedson Silva <contacanal798@gmail.com>
2022-09-22 10:40:24 -0300

    getting database password in _login method

--- database.py
+++ database.py
@@ -6,12 +6,11 @@ import requests
 class Database:
     def __init__(self) -> None:
         self.database_url = 'https://remote-cookiedb.herokuapp.com'
-        self._database_pw = os.environ.get('DATABASE_PW')
-
         self._token = self._login()
 
     def _login(self) -> str:
-        request = requests.get(self.database_url + '/login', params={'password': self.database_pw})
+        database_pw = os.environ.get('DATABASE_PW')
+        request = requests.get(self.database_url + '/login', params={'password': database_pw})
 
         if request.status_code == 401:
             raise ConnectionError('Invalid database API password')
