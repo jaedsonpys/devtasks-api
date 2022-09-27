5fa3621b496f42b846e5f5c728ff1992313ec8fd
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:30:44 -0300

    adding condition to run unit test

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,3 +9,7 @@ class TestAPI(bupytest.UnitTest):
         base_url = 'http://127.0.0.1:5550/api'
         self._login_url = f'{base_url}/login'
         self._register_url = f'{base_url}/register'
+
+
+if __name__ == '__main__':
+    bupytest.this()
