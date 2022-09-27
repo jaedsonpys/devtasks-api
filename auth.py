747f898954ec2b68fa728655d8a602f0c56776be
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:04:16 -0300

    adding create_user_token method to UserAuth

--- auth.py
+++ auth.py
@@ -1,6 +1,17 @@
 import os
+from datetime import datetime
+from datetime import timedelta
+
+import utoken
 
 
 class UserAuth:
     def _get_utoken_key(self) -> str:
         return os.environ.get('UTOKEN_KEY', 'secret-key')
+
+    def gen_user_token(self, email: str) -> str:
+        utoken_key = self._get_utoken_key
+        token_exp = datetime.now() + timedelta(minutes=5)
+
+        auth_token = utoken.encode({'email': email, 'max-time': token_exp}, utoken_key)
+        return auth_token
