4031a31ec0532d281a344dd266b393cfdce8489e
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:55:27 -0300

    fix api url in test

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ class TestAPI(bupytest.UnitTest):
     def __init__(self):
         super().__init__()
         
-        base_url = 'http://127.0.0.1:5550/api'
+        base_url = 'http://127.0.0.1:5500/api'
         
         self._login_url = f'{base_url}/login'
         self._register_url = f'{base_url}/register'
