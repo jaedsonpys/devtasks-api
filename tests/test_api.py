b63c76fbfa0945cf5eede9e365e59b896ce59ce1
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:40:12 -0300

    adding test test_register_user

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,6 +13,21 @@ class TestAPI(bupytest.UnitTest):
         self._user_email = 'user@mail.com'
         self._user_password = 'secret-password'
 
+    def test_register_user(self):
+        register_data = {
+            'email': self._user_email,
+            'password': self._user_password
+        }
+
+        request = requests.post(self._register_url, json=register_data)
+        self.assert_expected(request.status_code, 201)
+
+        result = request.json()
+
+        self.assert_expected(result.get('status'), 'success')
+        self.assert_expected(result.get('message'), 'Account created')
+        self.assert_true(result.get('token'))
+
 
 if __name__ == '__main__':
     bupytest.this()
