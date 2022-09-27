d9c9beafae29ad79950a22a8d0159ec1c82c9711
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:31:55 -0300

    adding user email and user password attributes

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,8 +7,11 @@ class TestAPI(bupytest.UnitTest):
         super().__init__()
         
         base_url = 'http://127.0.0.1:5550/api'
+        
         self._login_url = f'{base_url}/login'
         self._register_url = f'{base_url}/register'
+        self._user_email = 'user@mail.com'
+        self._user_password = 'secret-password'
 
 
 if __name__ == '__main__':
