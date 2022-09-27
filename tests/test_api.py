2306f6000ea4da0c5654dfb557e29483581ebf79
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:30:12 -0300

    creating unit test with BuPyTest library

---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+import bupytest
+import requests
+
+
+class TestAPI(bupytest.UnitTest):
+    def __init__(self):
+        super().__init__()
+        
+        base_url = 'http://127.0.0.1:5550/api'
+        self._login_url = f'{base_url}/login'
+        self._register_url = f'{base_url}/register'
