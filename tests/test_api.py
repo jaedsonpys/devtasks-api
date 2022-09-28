9e5b5e4e8c5c3c1c481467c6c5e6c72864cd786f
Jaedson Silva <contacanal798@gmail.com>
2022-09-28 20:06:44 -0300

    adding test test_register_exists_user

--- tests/test_api.py
+++ tests/test_api.py
@@ -28,6 +28,21 @@ class TestAPI(bupytest.UnitTest):
         self.assert_expected(result.get('message'), 'Account created')
         self.assert_true(result.get('token'))
 
+    def test_register_exists_user(self):
+        register_data = {
+            'email': self._user_email,
+            'password': self._user_password
+        }
+
+        request = requests.post(self._register_url, json=register_data)
+        self.assert_expected(request.status_code, 409)
+
+        result = request.json()
+
+        self.assert_expected(result.get('status'), 'error')
+        self.assert_expected(result.get('message'), 'User already exists')
+        self.assert_false(result.get('token'))
+
 
 if __name__ == '__main__':
     bupytest.this()
