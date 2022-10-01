eca87d188ada95347ef95d4d723db2e67706cb54
Jaedson Silva <contacanal798@gmail.com>
2022-10-01 10:22:48 -0300

    define token max time expiration to 2 minutes

--- src/auth.py
+++ src/auth.py
@@ -13,7 +13,7 @@ class UserAuth:
 
     def create_user_token(self, email: str) -> str:
         utoken_key = self._get_utoken_key()
-        token_exp = datetime.now() + timedelta(minutes=5)
+        token_exp = datetime.now() + timedelta(minutes=2)
 
         auth_token = utoken.encode({'email': email, 'max-time': token_exp}, utoken_key)
         return auth_token
