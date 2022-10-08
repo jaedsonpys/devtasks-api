ee35b0635e2c18e6b5740088ea357a225ec474d1
Jaedson Silva <contacanal798@gmail.com>
2022-10-08 15:38:08 -0300

    updating token time to 1 day

--- src/auth.py
+++ src/auth.py
@@ -14,7 +14,7 @@ class UserAuth:
 
     def create_user_token(self, email: str) -> str:
         utoken_key = self._get_utoken_key()
-        token_exp = datetime.now() + timedelta(minutes=2)
+        token_exp = datetime.now() + timedelta(days=1)
 
         auth_token = utoken.encode({'email': email, 'max-time': token_exp}, utoken_key)
         return auth_token
